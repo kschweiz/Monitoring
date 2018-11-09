99e0cfef357c4b1a74a16c8d95dba773bd4c7468
kschweiz <43673981+kschweiz@users.noreply.github.com>
2018-11-09 09:20:37 +0100

    Commit

--- DiskSpace/DiskSpace/Program.cs
+++ DiskSpace/DiskSpace/Program.cs
@@ -11,6 +11,7 @@ using System.Collections.Specialized;
 using System.Net.NetworkInformation;
 using System.Diagnostics;
 
+
 namespace DiskSpace
 {
     class Program
@@ -20,14 +21,15 @@ namespace DiskSpace
         public static PerformanceCounter ramCounter = new PerformanceCounter();
 
         //Fail / Bad / Error Counters
-        private static int countFailDisk = 0;
-        private static int countBadPing = 0;
-        private static int countBadUpDownLoad = 0;
+        public static int countFailDisk = 0;
+        public static int countBadPing = 0;
+        public static int countBadUpDownLoad = 0;
 
-        private static double traffic;
+        public static double traffic;
 
-        static int countDrives;
-        static NameValueCollection list = ConfigurationManager.AppSettings;
+        public static int countDrives;
+        public static NameValueCollection list = ConfigurationManager.AppSettings;
+
         static void Main(string[] args)
         {
             Console.WindowHeight = 30;
@@ -44,28 +46,18 @@ namespace DiskSpace
                 {
                     progressBar.MaxSteps = 100;
                     traffic = CheckWLANUsage(progressBar);
-                    //for(int i = 0; i<10; i++)
-                    //{
-                    //    Thread.Sleep(100);
-                    //    progressBar.StepForward();
-                    //}
-
                 }
                 Console.CursorLeft = 0;
                 Console.Write("Getting Information...");
 
-
                 Start();
                 UsageControl();
-                CheckDiskSize();
-                PingTest();
-                UpAndDownLoad();
+                DiskUsage.CheckDiskSize();
+                PingTest.CheckPing();
+                UpAndDownload.UpAndDownLoad();
                 PrintTraffic(traffic);
                 StatusLine();
             }
-
-
-
         }
 
         public static void StatusLine()
@@ -118,269 +110,11 @@ namespace DiskSpace
             Console.Write(DateTime.Now + "\n");
         }
 
-        public static void CheckDiskSize()
-        {
-            Console.CursorTop = 9;
-            Console.CursorLeft = 0;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("Disk Space List:");
-            DriveInfo[] Drives = DriveInfo.GetDrives(); // alle Laufwerke auslesen
-            countDrives = Drives.Count();
-            Console.CursorTop = 10;
-            Console.CursorLeft = 0;
-            foreach (DriveInfo d in Drives)
-            {
-                Console.Write(new string(' ', Console.WindowWidth));
-                Console.CursorLeft = 0;
-                string name = d.Name;
-                long totalSize;
-                long freeSize;
-                int prozent;
-                try
-                {
-                    totalSize = (d.TotalSize / (1024 * 1024 * 1024));
-                    freeSize = (d.TotalFreeSpace / (1024 * 1024 * 1024));
-                    prozent = Convert.ToInt32(100.00 / totalSize * (totalSize - freeSize));
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write($"Name: ");
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    Console.Write(name);
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write("\tStatus: ");
-                    if(prozent > 75)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write("CARE");
-                    }
-                    else if(prozent > 95)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write("FULL");
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write("OK");
-                    }
+        
 
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write("\tFree: ");
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.Write($"{freeSize} GB");
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write("\tFull: ");
-                    if(prozent > 75)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write($"{prozent}%\n");
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.Write($"{prozent}%\n");
-                    }
-                }
-                catch
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write($"Name: ");
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    Console.Write(name);
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write("\tStatus: ");
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write("FAILED");
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write("\tFree: ");
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.Write($"---");
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write("\tFull: ");
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.Write($"---\n");
-                    countFailDisk++;
-                }
+       
 
-            }
-        }
-
-        public static void PingTest()
-        {
-            Console.CursorTop = countDrives + 11;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Ping Test:");
-
-            foreach(string s in list.AllKeys)
-            {
-                try
-                {
-
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Ping ping = new Ping();
-                    PingReply pr = ping.Send(list.Get(s));
-                    Console.Write(new string(' ', Console.WindowWidth));
-                    Console.CursorLeft = 0;
-                    if (pr.Status == IPStatus.Success)
-                    {
-                        Console.Write($"Ping: ");
-                        Console.ForegroundColor = ConsoleColor.Gray;
-                        Console.Write(s);
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.Write("\tStatus: ");
-                        if (pr.RoundtripTime < 100)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.Write("OK");
-                        }
-                        else
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.Write("BAD");
-                            countBadPing++;
-                        }
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.Write("\tTime: ");
-                        if (pr.RoundtripTime < 100)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.Write($"{pr.RoundtripTime} ms\n");
-                        }
-                        else
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.Write($"{pr.RoundtripTime} ms\n");
-                        }
-                    }
-                    else
-                    {
-                        Console.Write($"Ping: ");
-                        Console.ForegroundColor = ConsoleColor.Gray;
-                        Console.Write(s);
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.Write("\tStatus: ");
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write("FAILED");
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.Write("\tTime: ");
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.Write($"---\n");
-                        countBadPing++;
-                    }
-                }
-                catch
-                {
-                    Console.Write(new string(' ', Console.WindowWidth));
-                    Console.CursorLeft = 0;
-                    Console.Write($"Ping: ");
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    Console.Write(s);
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write("\tStatus: ");
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write("FAILED");
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write("\tTime: ");
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.Write($"---\n");
-                    countBadPing++;
-                }
-                
-
-            }
-        }
-
-        public static void UpAndDownLoad()
-        {
-            Console.CursorTop = countDrives + list.Count + 13;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Up and Download:");
-
-
-            if (!NetworkInterface.GetIsNetworkAvailable())
-            {
-                Console.Write(new string(' ', Console.WindowWidth));
-                Console.CursorLeft = 0;
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write("I/F: ");
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.Write("Not available");
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write("\tStatus: ");
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("FAILED");
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write("\tUP: ");
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("---");
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write("\tDOWN: ");
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("---\n");
-                countBadUpDownLoad++;
-            }
-            else
-            {
-                NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
-                long teiler = Convert.ToInt64(1024 * 1024);
-
-                foreach (NetworkInterface ni in interfaces)
-                {
-                    if (ni.Name == "WiFi" || ni.Name == "Ethernet")
-                    {
-                        try
-                        {
-                            long up = ni.GetIPv4Statistics().BytesSent / teiler;
-                            long down = ni.GetIPv4Statistics().BytesReceived / teiler;
-                            Console.Write(new string(' ', Console.WindowWidth));
-                            Console.CursorLeft = 0;
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.Write("I/F: ");
-                            Console.ForegroundColor = ConsoleColor.Gray;
-                            Console.Write(ni.Name);
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.Write("\tStatus: ");
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.Write("OK");
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.Write("\tUP: ");
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.Write(up);
-                            Console.Write(" Mbps");
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.Write("\tDOWN: ");
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.Write(down);
-                            Console.Write(" Mbps\n");
-                        }
-                        catch
-                        {
-                            Console.Write(new string(' ', Console.WindowWidth));
-                            Console.CursorLeft = 0;
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.Write("I/F: ");
-                            Console.ForegroundColor = ConsoleColor.Gray;
-                            Console.Write(ni.Name);
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.Write("\tStatus: ");
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.Write("FAILED");
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.Write("\tUP: ");
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.Write("---");
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.Write("\tDOWN: ");
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.Write("---\n");
-                            countBadUpDownLoad++;
-                        }
-
-                    }
-                }
-
-            }
-
-        }
+        
 
         public static int CheckWLANUsage(ProgressBar progressBar)
         {
